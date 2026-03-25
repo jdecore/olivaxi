b854fc511cf7741901e4f3ddd8cb2d47005db9bd
jdenriquezr <jdenriquezr@gmail.com>
2026-03-25 18:33:49 -0500

    feat: add ViewTransitions and prefetch for instant navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  prefetch: true,
   devToolbar: {
     enabled: false
   }
