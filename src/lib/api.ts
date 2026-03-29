9ec9d37c50e3c578a8e87ebd79c4a62e94a34284
jdenriquezr <jdenriquezr@gmail.com>
2026-03-28 20:31:29 -0500

    fix: API URL to use same domain with /api path

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
 export const apiUrl = (path: string) => {
-  const cleanPath = path.replace(/\/$/, '');
+  const cleanPath = path.replace(/\/$/, '').replace(/^\/api/, '');
   return `http://45.90.237.135:3001/api${cleanPath}`;
 };
